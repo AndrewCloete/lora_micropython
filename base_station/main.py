836ba69fa08293d02930a2d3d7264ae7e1779dd0
Andrew Cloete <andrew.cloete@spatialedge.co.za>
2025-04-18 16:50:38 +0200

    Adds heartbeat (with watchdo) and refactors with base_station and field_node

---
+++ base_station/main.py
@@ -0,0 +1,35 @@
+import machine
+from machine import SPI, Pin
+from sx127x import SX127x
+import rx
+import time
+
+nss = Pin(8, Pin.OUT)  # Chip select
+reset = Pin(9, Pin.OUT)  # Reset pin
+dio0 = Pin(7, Pin.IN)  # IRQ pin
+dio1 = Pin(10, Pin.IN)  # Interrupt pin (if used)
+
+
+# Define SPI and pins based on your provided mapping
+spi = SPI(
+    0, baudrate=500000, polarity=0, phase=0, sck=Pin(18), mosi=Pin(19), miso=Pin(16)
+)
+pins = {
+    "ss": 8,  # NSS (CS)
+    "reset": 9,  # RESET (RST)
+    "dio0": 7,  # DIO0 (G0)
+    "dio1": 10,  # DIO1 (G1)
+}
+
+lora = SX127x(spi, pins, parameters={"frequency": 868000000})
+
+
+#SLEEP_DURATION = 10000  # 10 seconds
+#tx.sender(lora)
+#print("Going to deep sleep for 10 seconds...")
+#time.sleep(1)
+#machine.deepsleep(SLEEP_DURATION)
+
+wd = machine.WDT(timeout=8000)
+rx.receiver(lora, wd)
+
